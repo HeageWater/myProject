e03b6a4a3f108074b1959d2939aecaff32bdf2d9
KaoruMizugaki <k021g1249@g.neec.ac.jp>
2022-04-13 17:51:12 +0900

    頂点バッファビューの作成まで

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <cassert>
 #include <vector>
 #include <string>
+#include <DirectXMath.h>
+using namespace DirectX;
 
 #pragma comment(lib,"d3d12.lib")
 #pragma comment(lib,"dxgi.lib")
@@ -231,6 +233,78 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	UINT64 fenceVal = 0;
 
 	result = device->CreateFence(fenceVal, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence));
+
+
+
+	XMFLOAT3 vertices[] =
+	{
+		{-0.5f,-0.5f,0.0f},
+		{-0.5f,+0.5f,0.0f},
+		{+0.5f,-0.5f,0.0f},
+	};
+
+	//頂点データサイズ　= 頂点データサイズ一つ分 * 要素数
+	UINT sizeVB = static_cast<UINT>(sizeof(XMFLOAT3) * _countof(vertices));
+
+	//頂点バッファの設定
+
+	//ヒープの設定
+	D3D12_HEAP_PROPERTIES heapProp{};
+
+	//GPUへの転送用
+	heapProp.Type = D3D12_HEAP_TYPE_UPLOAD;
+
+	//リソース設定
+	D3D12_RESOURCE_DESC resDesc{};
+	resDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	resDesc.Width = sizeVB;
+	resDesc.Height = 1;
+	resDesc.DepthOrArraySize = 1;
+	resDesc.MipLevels = 1;
+	resDesc.SampleDesc.Count = 1;
+	resDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+	//頂点バッファの生成
+	ID3D12Resource* vertBuff = nullptr;
+	result = device->CreateCommittedResource(
+		//ヒープ設定
+		&heapProp,
+		D3D12_HEAP_FLAG_NONE,
+
+		//リソース設定
+		&resDesc,
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
+		IID_PPV_ARGS(&vertBuff));
+	assert(SUCCEEDED(result));
+
+	//gpu状のバッファに対応した仮想メモリ(メインメモリ上)を取得
+	XMFLOAT3* vertMap = nullptr;
+	result = vertBuff->Map(0, nullptr, (void**)&vertMap);
+	assert(SUCCEEDED(result));
+
+	//全頂点に対して
+	for (int i = 0; i < _countof(vertices); i++)
+	{
+		//座標コピー
+		vertMap[i] = vertices[i];
+	}
+
+	//つながりを削除
+	vertBuff->Unmap(0, nullptr);
+
+	//頂点バッファビューの作成
+	D3D12_VERTEX_BUFFER_VIEW vbView{};
+
+	//GPU仮想アドレス
+	vbView.BufferLocation = vertBuff->GetGPUVirtualAddress();
+
+	//頂点バッファのサイズ
+	vbView.SizeInBytes = sizeVB;
+
+	//頂点1つ分のデータサイズ
+	vbView.StrideInBytes = sizeof(XMFLOAT3);
+
 
 	//ゲームループ
 	while (true)
