5095a49c22c122dc709c4df3cbf7db7e7fa98d6f
KaoruMizugaki <k021g1249@g.neec.ac.jp>
2023-05-24 01:52:19 +0900

    push

--- Sprite.cpp
+++ Sprite.cpp
@@ -2,6 +2,15 @@
 
 Sprite::Sprite()
 {
+	//spriteCommon->dxCommon();
+
+	//	dxCommon = new DirectXCommon();
+	//dxCommon->Initialize();
+
+	//assert(dxCommon);
+
+	//this->dxCommon = *dxCommon;
+
 	//頂点データ構造体
 	struct Vertex
 	{
@@ -53,6 +62,37 @@ Sprite::Sprite()
 
 	//頂点データサイズ　= 頂点データサイズ一つ分 * 要素数
 	UINT sizeVB = static_cast<UINT>(sizeof(vertices[0]) * _countof(vertices));
+
+	//頂点バッファの設定
+	//ヒープの設定
+	D3D12_HEAP_PROPERTIES heapProp{};
+
+	//GPUへの転送用
+	heapProp.Type = D3D12_HEAP_TYPE_UPLOAD;
+
+	//リソース設定
+	D3D12_RESOURCE_DESC resDesc{};
+	resDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	resDesc.Width = sizeVB;
+	resDesc.Height = 1;
+	resDesc.DepthOrArraySize = 1;
+	resDesc.MipLevels = 1;
+	resDesc.SampleDesc.Count = 1;
+	resDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+	////頂点バッファの生成
+	//ID3D12Resource* vertBuff = nullptr;
+	//result = spriteCommon->dxCommon->GetDevice()->CreateCommittedResource(
+	//	//ヒープ設定
+	//	&heapProp,
+	//	D3D12_HEAP_FLAG_NONE,
+
+	//	//リソース設定
+	//	&resDesc,
+	//	D3D12_RESOURCE_STATE_GENERIC_READ,
+	//	nullptr,
+	//	IID_PPV_ARGS(&vertBuff));
+	//assert(SUCCEEDED(result));
 }
 
 Sprite::~Sprite()
