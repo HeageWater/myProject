d0708f170e422f0d3e6d589484649ba5aeb821c2
KaoruMizugaki <k021g1249@g.neec.ac.jp>
2023-07-06 11:30:05 +0900

    scene追加

--- Scene.cpp
+++ Scene.cpp
@@ -1,1 +1,34 @@
 #include "Scene.h"
+
+void Scene::Initialize()
+{
+	//player
+
+	//player->Initialize(dx.get(), shader, pipeline.get());
+
+	////enemy
+	//enemy->Initialize(dx.get(), shader, pipeline.get());
+	//enemy->SetTrans(Vector3D{ 100,20,0 });
+
+	//enemy2->Initialize(dx.get(), shader, pipeline.get());
+	//enemy2->SetTrans(Vector3D{ 300,40,0 });
+
+	//enemy3->Initialize(dx.get(), shader, pipeline.get());
+	//enemy3->SetTrans(Vector3D{ 600,30,0 });
+
+	//enemy4->Initialize(dx.get(), shader, pipeline.get());
+	//enemy4->SetTrans(Vector3D{ 800,50,0 });
+
+	////stage
+	//stage->Initialize(dx.get(), shader, pipeline.get());
+	//float minMapX = stage->stage_.mat.scale.x - 200;
+	//stage->stage_.mat.trans.x = minMapX;
+
+	//stageWhite->Initialize(dx.get(), shader, pipeline.get());
+	//stageWhite->stage_.mat.trans.y += 1;
+	//stageWhite->stage_.mat.scale.z = 10;
+	//stageWhite->Update(matView.mat, matProjection, input.get());
+
+	//goal->Initialize(dx.get(), shader, pipeline.get());
+
+}
--- Scene.h
+++ Scene.h
@@ -1,6 +1,39 @@
 #pragma once
+#include "Player.h"
+#include "Stage.h"
+#include "Enemy.h"
+#include "Sound.h"
+#include "Goal.h"
+#include "Collision.h"
+#include "JsonFileOpen.h"
+#include "Scene.h"
 
 class Scene
 {
+private:
+	//player
+	Player* player = new Player();
 
+	//enemy
+	Enemy* enemy = new Enemy();
+	Enemy* enemy2 = new Enemy();
+	Enemy* enemy3 = new Enemy();
+	Enemy* enemy4 = new Enemy();
+
+	//stage
+	Stage* stage = new Stage();
+
+	Stage* stageWhite = new Stage();
+
+	Goal* goal = new Goal();
+
+	/*int white = dx->LoadTextureGraph(L"Resources/white1x1.png");
+	int texP = dx->LoadTextureGraph(L"Resources/cube.jpg");
+	int brPng = dx->LoadTextureGraph(L"Resources/br.png");
+	size_t enemyPng = dx->LoadTextureGraph(L"Resources/ene/enemy.png");
+	int clearTex = dx->LoadTextureGraph(L"Resources/gameclear.png");*/
+
+public:
+
+	void Initialize();
 };
