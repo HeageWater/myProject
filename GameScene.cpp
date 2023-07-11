56a881f1c88fe22cd4f894de3cf98399758f0eb6
KaoruMizugaki <k021g1249@g.neec.ac.jp>
2023-07-11 10:40:38 +0900

    基盤追加

---
+++ GameScene.cpp
@@ -0,0 +1,17 @@
+#include "GameScene.h"
+
+void GameScene::Update()
+{
+}
+
+void GameScene::Initilize()
+{
+}
+
+void GameScene::Draw()
+{
+}
+
+void GameScene::Finalize()
+{
+}
--- main.cpp
+++ main.cpp
@@ -23,6 +23,7 @@
 #include "Goal.h"
 #include "Collision.h"
 #include "JsonFileOpen.h"
+#include "Easing.h"
 
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 {
