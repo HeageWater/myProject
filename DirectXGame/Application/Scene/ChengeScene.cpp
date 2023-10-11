1a4568bde478bb45d64611bcaa19e46b138d420a
KaoruMizugaki <k021g1249@g.neec.ac.jp>
2023-10-11 16:53:38 +0900

    コメント追加

--- DirectXGame/Application/Scene/ChengeScene.cpp
+++ DirectXGame/Application/Scene/ChengeScene.cpp
@@ -4,10 +4,9 @@
 
 void ChengeScene::Initialize(Matrix matProjection)
 {
+	//画像関係
 	spriteCommon->Inilialize(MyDirectX::GetInstance(), false);
-
 	sprite_->Inilialize(spriteCommon, &matProjection);
-
 	tex = MyDirectX::GetInstance()->LoadTextureGraph(L"Resources/sprite/blockNormal.png");
 
 	//scale用
@@ -16,9 +15,11 @@ void ChengeScene::Initialize(Matrix matProjection)
 	//pos用
 	float width = 1.5f;
 
+	//サイズ
 	sprite_->scale.x = Window::window_width * size;
 	sprite_->scale.y = Window::window_height * size;
 
+	//位置
 	sprite_->position.x = -Window::window_width * width;
 	sprite_->position.y = -Window::window_height;
 }
@@ -28,6 +29,7 @@ void ChengeScene::Draw(size_t tex_)
 	//ここから画像描画(y軸は-に)
 	sprite_->PreDraw();
 
+	//描画
 	sprite_->Draw(tex_);
 }
 
@@ -36,45 +38,54 @@ void ChengeScene::Draw()
 	//ここから画像描画(y軸は-に)
 	sprite_->PreDraw();
 
+	//描画
 	sprite_->Draw(tex);
 }
 
 void ChengeScene::Update()
 {
+	//移動時の最高フレーム
 	float maxTime = 60;
 
+	//プレイフラグがたっているか
 	if (isPlayFlag)
 	{
+		//加算
 		time++;
 
-		if (time > 30.0f)
-		{
-			sprite_->position.x = (float)Easing::EaseInOut(-2200.0f, -640.0f, time, maxTime);
-		}
-		else if (time == 31.0f)
-		{
-			SceneManager::GetInstance()->ChangeScene("PLAY");
-		}
-		else
-		{
-
-		}
-
-		if (time > maxTime)
-		{
-			Reset();
-		}
+		////こんな感じで組むこと
+		//if (time > 30.0f)
+		//{
+		//	sprite_->position.x = (float)Easing::EaseInOut(-2200.0f, -640.0f, time, maxTime);
+		//}
+		//else if (time == 31.0f)
+		//{
+		//	SceneManager::GetInstance()->ChangeScene("PLAY");
+		//}
+		//else
+		//{
+		//	sprite_->position.x = (float)Easing::EaseInOut(-2200.0f, -640.0f, time - 30, maxTime);
+		//}
 	}
 
+	//maxTimeを超えたらリセット
+	if (time > maxTime)
+	{
+		Reset();
+	}
+
+	//更新
 	sprite_->Update();
 }
 
+//リセット
 void ChengeScene::Reset()
 {
 	isPlayFlag = false;
 	time = 0;
 }
 
+//プレイフラグを外部からOnにする
 void ChengeScene::SetPlayFlag()
 {
 	if (!isPlayFlag)
@@ -94,6 +105,7 @@ void ChengeScene::SetPlayFlag()
 	}
 }
 
+//インスタンス化
 ChengeScene* ChengeScene::GetInstance()
 {
 	static ChengeScene chengeScene_;
