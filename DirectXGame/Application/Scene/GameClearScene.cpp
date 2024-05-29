def4c75510f427ba1bc4c55f1f1595b61616434a
KaoruMizugaki <k021g1249@g.neec.ac.jp>
2024-05-29 17:08:00 +0900

    imgui不具合修正

--- DirectXGame/Application/Scene/GameClearScene.cpp
+++ DirectXGame/Application/Scene/GameClearScene.cpp
@@ -5,6 +5,40 @@
 
 void GameClearScene::Initialize()
 {
+	//描画用行列
+	matView_.Init(Vector3D(0.0f, 60.0f, -50.0f), Vector3D(0.0f, 30.0f, 0.0f), Vector3D(0.0f, 1.0f, 0.0f));
+
+	//shader
+	shader_.Initizlize(L"Resources/shader/BasicVS.hlsl", L"Resources/shader/BasicPS.hlsl");
+	bilShader_.Initizlize(L"Resources/shader/VShader.hlsl", L"Resources/shader/PShader.hlsl");
+
+	//pipeline
+	pipeline_ = std::make_unique<GPipeline>();
+	pipeline_->Initialize(MyDirectX::GetInstance()->GetDev(), shader_);
+
+	//描画初期化
+	multipathPipeline_ = std::make_unique<GPipeline>();
+	multipathPipeline_->Initialize(MyDirectX::GetInstance()->GetDev(), bilShader_);
+
+	//背景のスクリーン(これが必要なので依存しないようにしたい)
+	screen_.Initialize(multipathPipeline_.get(), bilShader_);
+	screen_.obj_.trans_.z_ = 100.1f;
+	screen_.obj_.scale_ = { Window::window_width_ * 2,Window::window_height_ / 2,0.2f };
+
+	//透過するかどうか
+	normalSpriteCommon_->Inilialize(MyDirectX::GetInstance(), true);
+
+	//
+	ParticleManager::GetInstance()->Initalize();
+	ParticleManager::GetInstance()->SetCamera(matView_.mat_, matProjection_);
+	ParticleManager::GetInstance()->SetDraw(shader_, pipeline_.get());
+
+	whiteTex_ = MyDirectX::GetInstance()->LoadTextureGraph(L"Resources/sprite/white1x1.png");
+}
+
+void GameClearScene::Update()
+{
+
 #ifdef _DEBUG
 
 	//ImGui受付開始
@@ -43,45 +77,8 @@ void GameClearScene::Initialize()
 
 #endif _DEBUG
 
-	//描画用行列
-	matView_.Init(Vector3D(0.0f, 60.0f, -50.0f), Vector3D(0.0f, 30.0f, 0.0f), Vector3D(0.0f, 1.0f, 0.0f));
-
-	//shader
-	shader_.Initizlize(L"Resources/shader/BasicVS.hlsl", L"Resources/shader/BasicPS.hlsl");
-	bilShader_.Initizlize(L"Resources/shader/VShader.hlsl", L"Resources/shader/PShader.hlsl");
-
-	//pipeline
-	pipeline_ = std::make_unique<GPipeline>();
-	pipeline_->Initialize(MyDirectX::GetInstance()->GetDev(), shader_);
-
-	//描画初期化
-	multipathPipeline_ = std::make_unique<GPipeline>();
-	multipathPipeline_->Initialize(MyDirectX::GetInstance()->GetDev(), bilShader_);
-
-	//背景のスクリーン(これが必要なので依存しないようにしたい)
-	screen_.Initialize(multipathPipeline_.get(), bilShader_);
-	screen_.obj_.trans_.z_ = 100.1f;
-	screen_.obj_.scale_ = { Window::window_width_ * 2,Window::window_height_ / 2,0.2f };
-
-	//透過するかどうか
-	normalSpriteCommon_->Inilialize(MyDirectX::GetInstance(), true);
-
-	//
-	ParticleManager::GetInstance()->Initalize();
-	ParticleManager::GetInstance()->SetCamera(matView_.mat_, matProjection_);
-	ParticleManager::GetInstance()->SetDraw(shader_, pipeline_.get());
-
-	whiteTex_ = MyDirectX::GetInstance()->LoadTextureGraph(L"Resources/sprite/white1x1.png");
-}
-
-void GameClearScene::Update()
-{
 	//スクリーン更新
 	screen_.MatUpdate(matView_.mat_, matProjection_, 0);
-
-
-	//
-	controller_->Update();
 
 	//パーティクル更新
 	ParticleManager::GetInstance()->SetCamera(matView_.mat_, matProjection_);
--- DirectXGame/Application/Scene/GameOverScene.cpp
+++ DirectXGame/Application/Scene/GameOverScene.cpp
@@ -4,6 +4,38 @@
 
 void GameOverScene::Initialize()
 {
+	//描画用行列
+	matView_.Init(Vector3D(0.0f, 60.0f, -50.0f), Vector3D(0.0f, 30.0f, 0.0f), Vector3D(0.0f, 1.0f, 0.0f));
+
+	//
+	whiteTex_ = MyDirectX::GetInstance()->LoadTextureGraph(L"Resources/sprite/white1x1.png");
+
+	//shader
+	shader_.Initizlize(L"Resources/shader/BasicVS.hlsl", L"Resources/shader/BasicPS.hlsl");
+	bilShader_.Initizlize(L"Resources/shader/VShader.hlsl", L"Resources/shader/PShader.hlsl");
+
+	//pipeline
+	pipeline_ = std::make_unique<GPipeline>();
+	pipeline_->Initialize(MyDirectX::GetInstance()->GetDev(), shader_);
+
+	//描画初期化
+	multipathPipeline_ = std::make_unique<GPipeline>();
+	multipathPipeline_->Initialize(MyDirectX::GetInstance()->GetDev(), bilShader_);
+
+	//背景のスクリーン(これが必要なので依存しないようにしたい)
+	screen_.Initialize(multipathPipeline_.get(), bilShader_);
+	screen_.obj_.trans_.z_ = 100.1f;
+	screen_.obj_.scale_ = { Window::window_width_ * 2,Window::window_height_ / 2,0.2f };
+
+	//透過するかどうか
+	normalSpriteCommon_->Inilialize(MyDirectX::GetInstance(), true);
+
+	//基礎
+	sprite_->Inilialize(normalSpriteCommon_, &matProjection_);
+}
+
+void GameOverScene::Update()
+{
 #ifdef _DEBUG
 
 	//ImGui受付開始
@@ -42,43 +74,11 @@ void GameOverScene::Initialize()
 
 #endif _DEBUG
 
-	//描画用行列
-	matView_.Init(Vector3D(0.0f, 60.0f, -50.0f), Vector3D(0.0f, 30.0f, 0.0f), Vector3D(0.0f, 1.0f, 0.0f));
+	//カメラ更新
+	matView_.MatUpdate();
 
-	//
-	whiteTex_ = MyDirectX::GetInstance()->LoadTextureGraph(L"Resources/sprite/white1x1.png");
-
-	//shader
-	shader_.Initizlize(L"Resources/shader/BasicVS.hlsl", L"Resources/shader/BasicPS.hlsl");
-	bilShader_.Initizlize(L"Resources/shader/VShader.hlsl", L"Resources/shader/PShader.hlsl");
-
-	//pipeline
-	pipeline_ = std::make_unique<GPipeline>();
-	pipeline_->Initialize(MyDirectX::GetInstance()->GetDev(), shader_);
-
-	//描画初期化
-	multipathPipeline_ = std::make_unique<GPipeline>();
-	multipathPipeline_->Initialize(MyDirectX::GetInstance()->GetDev(), bilShader_);
-
-	//背景のスクリーン(これが必要なので依存しないようにしたい)
-	screen_.Initialize(multipathPipeline_.get(), bilShader_);
-	screen_.obj_.trans_.z_ = 100.1f;
-	screen_.obj_.scale_ = { Window::window_width_ * 2,Window::window_height_ / 2,0.2f };
-
-	//透過するかどうか
-	normalSpriteCommon_->Inilialize(MyDirectX::GetInstance(), true);
-
-	//基礎
-	sprite_->Inilialize(normalSpriteCommon_, &matProjection_);
-}
-
-void GameOverScene::Update()
-{
 	//スクリーン更新
 	screen_.MatUpdate(matView_.mat_, matProjection_, 0);
-
-	//
-	controller_->Update();
 
 	//シーンチェンジ更新
 	ChengeScene::GetInstance()->Update();
